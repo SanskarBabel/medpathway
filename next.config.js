138f84d558300c41e1eccce37deed89712a4a738
SanskarBabel <sanskarbabel7742@gmail.com>
2025-10-06 22:32:48 +0530

    Some Commit

--- next.config.js
+++ next.config.js
@@ -1,21 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'docs',
-  eslint: { ignoreDuringBuilds: true },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     unoptimized: true,
-    formats: ['image/webp','image/avif'],
-    deviceSizes: [640,750,828,1080,1200,1920,2048,3840],
-    imageSizes: [16,32,48,64,96,128,256,384]
   },
   trailingSlash: true,
-  compress: true,
-  swcMinify: true,
-  experimental: {
-    optimizeCss: true,
-    optimizePackageImports: ['lucide-react']
-  }
+  basePath: '',
+  assetPrefix: '',
 };
 
 module.exports = nextConfig;
